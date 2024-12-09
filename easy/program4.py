df9001e1f976ba8452e1825f202bc6e7e581348c
Prathamesh Karande <148135010+KarandePrathamesh@users.noreply.github.com>
2024-12-09 22:44:39 +0530

    adding program 4

---
+++ easy/program4.py
@@ -0,0 +1,15 @@
+# Define a dictionary with at least 3 key-value pairs and retrieve a value using its key.
+
+myDict= {"name":"Prathamesh" , "age":21 , "address":"Satara"} 
+
+print('\nPriting all values from Dictionary :')
+print(myDict)
+
+print('\nPriting all values from Dictionary :')
+for d in myDict:
+    print(myDict[d])
+
+print('\nPrinting Specific values form the list :')
+print(myDict["name"])
+print(myDict["age"])
+print(myDict["address"])
